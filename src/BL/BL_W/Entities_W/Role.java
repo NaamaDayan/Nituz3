2348f96d8df43b36c6ae38d5dd40685a02f60b0d
dangvili <dangvili190698@gmail.com>
2018-05-26 16:01:58 +0300

    added role description

--- src/BL/BL_W/Entities_W/Role.java
+++ src/BL/BL_W/Entities_W/Role.java
@@ -3,8 +3,27 @@ package BL.BL_W.Entities_W;
 public class Role {
 
     private String role;
+    private String roleDesc;
 
-    public Role(String role) {
+
+    /**
+     * this constructor is used when the admin wants to register a new role that doesn't exist
+     * in the database. in this scenario he has to enter the role description as well
+     * @param role
+     * @param roleDesc
+     */
+    public Role(String role, String roleDesc) {
+        this.role = role;
+        this.roleDesc = roleDesc;
+    }
+
+    /**
+     * this constructor is used to initialize a role that is already in the database
+     * meaning this constructor creates a container for the roleName without the use
+     * of role description
+      * @param role
+     */
+    public Role(String role){
         this.role = role;
     }
 
@@ -14,9 +33,10 @@ public class Role {
 
     @Override
     public boolean equals(Object obj) {
-        if(!(obj instanceof Role))
+        if (!(obj instanceof Role))
             return false;
-        Role other = (Role)obj;
-        return this.role.toLowerCase().equals(other.role.toLowerCase());
+        Role other = (Role) obj;
+        return this.role.toLowerCase().equals(other.role.toLowerCase()) &&
+                this.roleDesc.toLowerCase().equals(other.roleDesc.toLowerCase());
     }
 }
--- src/PL/PL_W/insertCommand/InsertRole.java
+++ src/PL/PL_W/insertCommand/InsertRole.java
@@ -4,16 +4,32 @@ import BL.BL_W.RolesLogic;
 import BL.BL_W.Entities_W.Role;
 import PL.PL_W.Command;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class InsertRole implements Command{
-    static Scanner reader = new Scanner(System.in);
+    //static Scanner reader = new Scanner(System.in);
     @Override
     public void execute() {
         System.out.println("Enter the new role you would like to add:");
-        String roleName = reader.nextLine();
-        Role role = new Role(roleName);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String roleName = null;
+        try {
+            roleName = br.readLine();
+        } catch (IOException e) {
+        }
+
+        String roleDesc = null;
+        System.out.println("Enter the description of the role you just entered:");
+        try {
+            roleDesc= br.readLine();
+        } catch (IOException e) {
+        }
+
+        Role role = new Role(roleName, roleDesc);
         try {
             if(RolesLogic.roleExists(role))
                 System.out.println("The role "+role.getRole()+" already exists\n");
